39d6bcdd8156523322dfc6d91e84396c21af143a
Greenscarf <greenscarf005@gmail.com>
2020-05-19 05:31:09 +0900

    tutorial stage 2 complete

--- js/tile.js
+++ js/tile.js
@@ -1,1 +1,54 @@
-// 나중에 추가할거
+class Tile{
+	constructor(x, y, sprite, passable){
+        this.x = x;
+        this.y = y;
+        this.sprite = sprite;
+        this.passable = passable;
+	}
+
+	draw(){
+        drawSprite(this.sprite, this.x, this.y);
+    }
+
+    getNeighbor(dx, dy){
+        return getTile(this.x + dx, this.y + dy)
+    }
+
+    getAdjacentNeighbors(){
+        return shuffle([
+            this.getNeighbor(0, -1),
+            this.getNeighbor(0, 1),
+            this.getNeighbor(-1, 0),
+            this.getNeighbor(1, 0)
+        ]);
+    }
+
+    getAdjacentPassableNeighbors(){
+        return this.getAdjacentNeighbors().filter(t => t.passable);
+    }
+
+    getConnectedTiles(){
+        let connectedTiles = [this];
+        let frontier = [this];
+        while(frontier.length){
+            let neighbors = frontier.pop()
+                                .getAdjacentPassableNeighbors()
+                                .filter(t => !connectedTiles.includes(t));
+            connectedTiles = connectedTiles.concat(neighbors);
+            frontier = frontier.concat(neighbors);
+        }
+        return connectedTiles;
+    }
+}
+
+class Floor extends Tile{
+    constructor(x,y){
+        super(x, y, 2, true);
+    };
+}
+
+class Wall extends Tile{
+    constructor(x, y){
+        super(x, y, 3, false);
+    }
+}
